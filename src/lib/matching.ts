03a2c00868d4ef824daff3068dfd503b1e7909f0
georgesinati123534 <georgesinati@yahoo.com>
2025-11-25 16:37:10 +0000

    Generated by Spark: faire les horaires de 8h du matin jusqua 11h du soir

--- src/lib/matching.ts
+++ src/lib/matching.ts
@@ -2,8 +2,7 @@ import { Tutor, Tutee, Match, TimeSlot, Subject } from './types'
 
 export const DAYS = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
 export const TIME_SLOTS = [
-  '0h', '1h', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', '11h',
-  '12h', '13h', '14h', '15h', '16h', '17h', '18h', '19h', '20h', '21h', '22h', '23h'
+  '8h', '9h', '10h', '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h', '19h', '20h', '21h', '22h', '23h'
 ]
 
 export const SUBJECTS = [
